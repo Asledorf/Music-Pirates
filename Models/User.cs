ae0a1bda940e9f07b6070b56e35524eb0eb60d1e
CreativeBrain01 <brankin@student.neumont.edu>
2021-02-19 18:40:39 -0700

    Added followers, liked and disliked. etc.

--- Models/User.cs
+++ Models/User.cs
@@ -7,11 +7,17 @@ namespace Music_Pirates.Models
 {
     public class User
     {
+        public static List<User> Users = new List<User>();
         public Guid ID { get; set; }
         public string ImgURL { get; set; }
-        public static List<User> Users = new List<User>();
         public string Username { get; set; }
         public string Password { get; set; } //Will likely need to setup some security stuffs later
         public DateTime Birthday { get; set; }
+
+        protected List<User> Following = new List<User>();
+        protected List<User> Followers = new List<User>();
+        protected List<Playlist> Playlists = new List<Playlist>();
+        protected List<Song> Liked = new List<Song>();
+        protected List<Song> Disliked = new List<Song>();
     }
 }
